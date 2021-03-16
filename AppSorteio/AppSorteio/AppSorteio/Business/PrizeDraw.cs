9d9ea1bda251e6a9083fe706d04e9b88f07254f4
Paulo Henrique Maestro <paulomaestro@employer.com.br>
2021-03-15 22:16:16 -0300

    #ENC - Create prizedraw

---
+++ AppSorteio/AppSorteio/AppSorteio/Business/PrizeDraw.cs
@@ -0,0 +1,45 @@
+﻿using AppSorteio.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AppSorteio.Business
+{
+    public class PrizeDraw
+    {
+        private List<Person> LstPerson { get; set; } 
+        public PrizeDraw()
+        {
+            var file = new System.IO.StreamReader(@"Import/rd-http-adrianobinhara-com-br-lp-pre-lancamento-de-curso.csv");
+            var line = "";
+            var counter = 0;
+            while ((line = file.ReadLine()) != null)
+            {
+                if (counter  == 0)
+                {
+                    counter++;
+                    continue;
+                }
+                var splitLine = line.Split(',');
+                LstPerson.Add(new Person()
+                {
+                    Index = counter,
+                    Email = splitLine[0],
+                    Name = splitLine[1],
+                    UserName = splitLine[2]
+                });
+                counter++;
+            }
+
+            file.Close();
+        }
+        public Person PrizeDrawPerson()
+        {
+            var ran = new Random(LstPerson.Max(m => m.Index)).Next();
+            var result = LstPerson.Where(w => w.Index == ran).FirstOrDefault();
+            return result;
+        }
+    }
+}
